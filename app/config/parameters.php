1e636060d3b529218de57a7746eff830f4af1325
Gilberto Hernandez <ghernandez.9002@gmail.com>
2025-07-12 05:00:56 -0600

    [Init] Initial project setup

---
+++ app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'u648654646_autopartes',
+    'database_user' => 'u648654646_admin',
+    'database_password' => 'AutopartesDago.1792@',
+    'database_prefix' => 'aut_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'GvunUvGrzKDkRHBz4ct8EmMRnb67aDQxy8gy8WyWDD9IKWPSVkF4KIRSWQiDXXQI',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2025-07-07',
+    'locale' => 'es-ES',
+    'cookie_key' => 'ZnyxWr0aITKMaJOWas3fPCrz6sAiTt4qV0ZpbDJGZRjsx9dgVkgd8kLj7zNAptoq',
+    'cookie_iv' => 'PIvSSfQI1WMu2nUZup5MIHSq9VYyvmTP',
+    'use_debug_toolbar' => true,
+    'new_cookie_key' => 'def0000019bb27667b26147e24a1722a12a57e7f176433996fdc7433bcbe4650f52a251f4ace5303c0f11a661e8bc9dbe2a392cceb9f8a2ba9c476eef3331eb791a6ea0e',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0uLLZlIfJvte++h3ed9w
+hOFRqivaEapOmzWlBNgGopmENdrT39/G1DRYcNbA1MwdiH5IN9drcYwpWBeTZUtl
+PKLF6UOgOpUHvODGHYdSD9wXl4oBIHsDXojfO0QFZggE4Sj8s8crf/kfjscRHMrr
+mcP3Gr1LMJCnVMYjMl7ylmSa1gGwQGqHTAqYTufTmg+S4mC/NJmaTksyOuhsyani
+4cHPdugo4Lf2gGecP74uyNu4THGvvC35eE8IfBYA7sBbY0RSEIOs7NIRb2WpbzcP
+ZaeE2oVBvCMAQWwxrtITXtjbPtusAFAS73X1Ww2H8Brav151SUsoCPB15UCOaGtG
+9wIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDS4stmUh8m+177
+6Hd533CE4VGqK9oRqk6bNaUE2AaimYQ12tPf38bUNFhw1sDUzB2Ifkg312txjClY
+F5NlS2U8osXpQ6A6lQe84MYdh1IP3BeXigEgewNeiN87RAVmCAThKPyzxyt/+R+O
+xxEcyuuZw/cavUswkKdUxiMyXvKWZJrWAbBAaodMCphO59OaD5LiYL80mZpOSzI6
+6GzJqeLhwc926Cjgt/aAZ5w/vi7I27hMca+8Lfl4Twh8FgDuwFtjRFIQg6zs0hFv
+ZalvNw9lp4TahUG8IwBBbDGu0hNe2Ns+26wAUBLvdfVbDYfwGtq/XnVJSygI8HXl
+QI5oa0b3AgMBAAECggEBAJLQExxbxZZE375t+2dcF6BaoJjgyIMN1oWVVhnGLvBa
+AcHgYypzRp+LBZsg1sQOiezxRWOi1AkLs+jBGPsm/wa0pyWaz9yYzfc/YI1EHOwv
+pDj6eYhcO18/2HERDNUYdhUujKa33N/N1pt2mMI7vsOgutHhwIvB2Mxa2DwD/8yU
+XnhVqXIANwxIgYOv7ANrU7rIJKQ4df9c3jSPmXtSsVrZJ/CsX4miqvC5Ylhx6Mek
+w6r8ISnSY7JRSBVg2H3aoD5G512mYalf+8s9NUBaQiiJoIs85VjIKiJghiyNWLVK
+zUG9z4+2eL/hIy+o+p/+6jvhqXXTTyP9uP7Llen5pSkCgYEA/8YXkhgXbV2cbmX2
+B+tlgeP6PhqZWVaJsmfsfmHls0x8pMGLM2lL5PMgcph4mAI0AKThmM2Fp0UPKxq9
+E/wKaMAZqhdkWiFSSzEwERCtSHTHuMyx88VBKMbMvZ1EMhnVBvEIR69Rl4xahdZu
+fGkmfu6MW82UfIxN6MYqWqZmQysCgYEA0xKKKoDmr6JzAGHUhmKRC9kPaJuRlP4u
+YHTcgnkuiazbFOTdHhnbtlIyyIdWo7B3Y3JZgs2faHGU7LYYfuQMc4+YQzsaYN/K
+U4UkoWk39fqN55lVdWwl+0668anx57I3202t8r9lJhIOuIdrkvhh9MtFpOPxq1bb
+bfu6k6yQ1WUCgYBCCcpiLRg3Y5kVI/w/FTcTL2KYEFQRrfdFefGku/ALuC7idxpS
+vtgal++Dxvwz+zWkCO46fq4IfxsofPHerA+f9iFqOjntz3ao9V08urqS35wFicxU
+bVSii5mnR2ipKuRGs4r4vzpoz5M7xR6nvppy4XKZ7V1uPueKt0eOOjJjGwKBgQCT
+rvIrgRtnonNyvgC0snyoXIyydb8sD5kldj7fr6iHPgHgIa4xJIidje/obQGK8Kro
+UxccNYEm0+oOoe6LwkaQCNXRnKX9SDhJrewOYA9+tXvao2evDCebKeAVvQH8Sq9Y
+a4adRZuhQps4M9Bp+g6X+C5XjAIJD3HjB3HpZ6HDHQKBgQDJEw+hPJvDid0DrOlM
+kAR+317RtPqGRT6+j+ArsC2yLHXuaoYzolakBphbSYpZZ9eqHxlm8FdyOijoH7hw
+ElEaIx4DB0xokSsofr6C2ENB6VXcAiaAaMzjZBTvdLua5SNIFma6u48IY1wlrP3a
+6MEoj1amZoGodr/PdRj/jsXlLg==
+-----END PRIVATE KEY-----
+',
+  ),
+);
